df030977c983a28935ecca98100797d988c18377
Aaronqin <Aaronqcd@gmail.com>
2016-02-26 18:54:39 +0800

    实现登录与注册页面的切换功能

--- app/src/main/java/com/ewangz/imitatewechat/LoginActivity.java
+++ app/src/main/java/com/ewangz/imitatewechat/LoginActivity.java
@@ -1,7 +1,11 @@
 package com.ewangz.imitatewechat;
 
+import android.content.ComponentName;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,6 +13,16 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        //Button lo
+        TextView register = (TextView)findViewById(R.id.registerLink);
+        register.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                ComponentName componentname = new ComponentName(LoginActivity.this, RegisterActivity.class);
+                Intent intent = new Intent();
+                intent.setComponent(componentname);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/ewangz/imitatewechat/RegisterActivity.java
+++ app/src/main/java/com/ewangz/imitatewechat/RegisterActivity.java
@@ -1,7 +1,11 @@
 package com.ewangz.imitatewechat;
 
+import android.content.ComponentName;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        TextView loginLink = (TextView)findViewById(R.id.returnLoginLink);
+        loginLink.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                ComponentName componentname = new ComponentName(RegisterActivity.this, LoginActivity.class);
+                Intent intent = new Intent();
+                intent.setComponent(componentname);
+                startActivity(intent);
+            }
+        });
     }
 }
